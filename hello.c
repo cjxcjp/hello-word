f456460975cb7e5f232e4392688416f645292969
cjxcjp <55731920+cjxcjp@users.noreply.github.com>
2019-10-15 21:53:50 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -8,9 +8,9 @@
 curl=curl_easy_init();
  if(curl){
  curl_easy_setopt(curl,CURLOPT_URL, "http://myip.ipip.net/") ;
- res=curl_easy_perform(curl)；
+ res=curl_easy_perform(curl);
  if(res!=CURLE_OK)
- fprintf(stderr, "curl_easy_perform（）failed: %s\n",curl_easy_strerror(res));
+ fprintf(stderr, "curl_easy_perform() failed:%s\n",curl_easy_strerror(res));
  curl_easy_cleanup(curl);
  }
  return 0;
